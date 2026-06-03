da9cb173afc3323b3e6cada364c40c48265d9372
leepeter99 <leepeterwy@gmail.com>
2026-06-03 22:45:25 +0800

    newer stylix manages gtk4.theme

--- modules/home/gtk.nix
+++ modules/home/gtk.nix
@@ -1,6 +1,13 @@
-{config, pkgs, ...}: {
+{
+  config,
+  pkgs,
+  lib,
+  ...
+}: {
   gtk = {
-    gtk4.theme = null;
+    # Newer Stylix manages gtk4.theme; force-null keeps gtk4 unthemed (prior behavior).
+    # Delete this mkForce line instead if you want Stylix to theme gtk4 apps too.
+    gtk4.theme = lib.mkForce null;
     iconTheme = {
       name = "Papirus-Dark";
       package = pkgs.papirus-icon-theme;
